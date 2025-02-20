0ce6b5c9e8fb517a28e6794c2cc8ed0f24ba2473
Stupel <marian.holly7117@gmail.com>
2025-02-20 20:54:40 +0100

    Update StateSwitcher.tsx

--- src/components/StateSwitcher.tsx
+++ src/components/StateSwitcher.tsx
@@ -1,24 +1,40 @@
 export default function StateSwitcher({
   handleClick,
- 
 }: {
-  handleClick: (state: string) => void
-  
+  handleClick: (state: string) => void;
 }) {
+  let states = ["default", "warning", "danger"];
+
   return (
     <div
       style={{
         width: "100%",
-        padding: "35px",
+        margin: "35px",
         display: "flex",
         justifyContent: "center",
         flexDirection: "row",
         gap: "5px",
+        borderBottom: "2px solid #f4f4f4",
       }}
     >
-      <button onClick={() => handleClick("default")}>Default</button>
-      <button onClick={() => handleClick("warning")}>Warning</button>
-      <button onClick={() => handleClick("danger")}>Danger</button>
+      {states.map((state) => {
+        return (
+          <button
+            style={{
+              backgroundColor: "#f4f4f4",
+              padding: "4px",
+              marginBottom: "10px",
+              border: "none",
+              borderRadius: "5px",
+              cursor: "pointer",
+              textTransform: 'capitalize',
+            }}
+            onClick={() => handleClick(state)}
+          >
+            {state}
+          </button>
+        );
+      })}
     </div>
   );
 }
