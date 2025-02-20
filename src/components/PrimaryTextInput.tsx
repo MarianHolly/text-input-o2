8f041b6d87777ff6e13b06430cf4d07acb77dba8
Stupel <marian.holly7117@gmail.com>
2025-02-20 16:56:55 +0100

    html structure of component

--- src/components/PrimaryTextInput.tsx
+++ src/components/PrimaryTextInput.tsx
@@ -1,5 +1,14 @@
 export default function PrimaryTextInput() {
   return (
-    <div>Primary Text Input</div>
-  )
+    <div className="text-input">
+      <div className="header">
+        <div className="label">
+          <label className="label-text">Label</label>
+          <label className="label-optional">Optional</label>
+        </div>
+        <label className="label-description">Description</label>
+      </div>
+      <input type="text" className="input-form" placeholder="Placeholder" />
+    </div>
+  );
 }
